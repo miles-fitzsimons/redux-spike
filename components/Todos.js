f0dfd787f864ac0400575ff33125f21afd8383e0
miles-fitzsimons <miles.fitzsimons@gmail.com>
2016-07-11 14:47:03 +1200

    started to do reducer

---
+++ components/Todos.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Todos extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Hello</h1>
+			</div>
+		)
+	}
+
+
+}
+
+export default Todos
--- index.js
+++ index.js
@@ -2,18 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Counter from './components/Counter'
 import counter from './reducers/counter'
+import Todos from './components/Todos'
+import todos from './reducers/todos'
 import { createStore } from 'redux'
 
 const store = createStore(counter) // reducer counter bound to store
 
 function render () {
 	ReactDOM.render(
-		<Counter
-			value={store.getState()}
-			onIncrement={() => store.dispatch({type: 'INCREMENT'})}
-			onDecrement={() => store.dispatch({type: 'DECREMENT'})}
-
-		/>,
+		<Todos />,
 		document.querySelector('main')
 	)
 }
